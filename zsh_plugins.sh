97bbe62fea449a508f55e6220e4a11baa5174713
Tony Do <tdo@kopismobile.com>
2018-10-09 00:05:24 -0500

    fixing install directory for zsh-256color plugin

--- zsh_plugins.sh
+++ zsh_plugins.sh
@@ -1,6 +1,6 @@
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.file/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/chrissicool/zsh-256color
+git clone https://github.com/chrissicool/zsh-256color ~/.file/oh-my-zsh/custom/plugins/zsh-256color
 
 # powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.file/oh-my-zsh/custom/themes/powerlevel9k
