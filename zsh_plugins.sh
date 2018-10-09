d401d0b8556db5c44064f781eb823722ee2807e5
Tony Do <tdo@kopismobile.com>
2018-10-08 23:47:57 -0500

    adding zsh plugins

---
+++ zsh_plugins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.file/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/chrissicool/zsh-256color
+
+# powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.file/oh-my-zsh/custom/themes/powerlevel9k
