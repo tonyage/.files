13e5f2dc12f715f65aee28ff9cfa16ce56924d6a
Tony Do <tdo@kopismobile.com>
2018-11-19 13:36:11 -0600

    adding silversearcher package and cleaning up linux install script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-sudo apt install wget curl make cmake build-essential gcc g++ python3-dev binutils 
-sudo apt isntall bzip2 libncurses5-dev libncursesw5-dev 
+sudo apt install wget / 
+	curl / 
+	make /
+	cmake /
+	build-essential /
+	gcc /
+	g++ /
+	python3-dev /
+	binutils / 
+	bzip2 /
+	libncurses5-dev /
+	libncursesw5-dev /
+	silversearcher-ag 
