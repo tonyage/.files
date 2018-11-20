2647dca60db47839d19992b3c60ec9bc19c6a85f
Tony Do <ttdo@bitbucket.com>
2018-11-20 01:12:00 -0600

    tmux initial config

--- symlink_rc.sh
+++ symlink_rc.sh
@@ -1,6 +1,7 @@
 rm $HOME/.zshrc
 rm $HOME/.vimrc
 rm $HOME/.bashrc  
+rm $HOME/.tmux.conf 
 
 rm -rf $HOME/.vim
 rm -rf $HOME/.config/powerline 
@@ -9,4 +10,5 @@ ln -s $HOME/.file/.vim $HOME/.vim
 ln -s $HOME/.file/.zshrc $HOME/.zshrc
 ln -s $HOME/.file/.vimrc $HOME/.vimrc
 ln -s $HOME/.file/.bashrc $HOME/.bashrc
+ln -s $HOME/.file/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.file/.config/powerline $HOME/.config/powerline 
---
+++ tmux_install.sh
@@ -0,0 +1,1 @@
+tic -x tmux.terminfo
