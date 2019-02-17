516d09a0783a8e47766152f70edbe243da95f078
Tony Do <tdo@kopismobile.com>
2019-02-16 21:43:48 -0800

    script that symlinks stuff for me cleaning up install.sh a little bit

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -1,8 +1,34 @@
-mkdir ~/.config
+#!/bin/bash
 
-ln -s ~/.file/.vim ~/.vim
+DIR="$HOME/.file"
+dotfiles=($DIR/.vim $DIR/.vimrc $DIR/.zshrc $DIR/.gitconfig)
 
-ln -s ~/.file/.zshrc ~/.zshrc
-ln -s ~/.file/.vimrc ~/.vimrc
+mkdir ~/.config > /dev/null 2>&1
 
-ln -s ~/.file/.vim ~/.config/nvim
+for d in "${dotfiles[@]}"; do
+	ln -s $d ~/$d > /dev/null 2>&1
+	if [ $d == ${dotfiles[0]} ]; then 
+		ln -s $d ~/.config/nvim 2>&1
+	fi
+done
+
+# function to test symlink quality
+symcheck() {
+	file=$1
+	if [ -L $file ]; then
+		if [ -e $file ]; then
+			echo "$file symlink is functional"
+		else
+			echo "$file link broken"
+		fi
+	elif [ -e $file ]; then
+		echo "Not a valid symlink"
+	else 
+		echo "$file is missing"
+	fi
+}
+
+file=(~/.vim ~/.vimrc ~/.zshrc ~/.gitconfig ~/.config/nvim)
+for f in "${file[@]}"; do
+	symcheck $f
+done
