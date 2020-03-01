32854863d8c2dc5093ec89c1c5aac3786396c4fa
Tony Do <tdo@kopismobile.com>
2020-03-01 13:19:06 -0600

    typo in bash shebang was causing misfires trying to run script

--- bin/install.sh
+++ bin/install.sh
@@ -1,27 +1,25 @@
-#/bin/bash
+#!/bin/bash
 
 read -p "Would you like to use zsh or fish? " ans
-input=${ans,}
 
-if [[ "$input" == "zsh" ]]; then
-	if [[ "$(uname)" == "Linux" ]]; then
-		if [[ "$(lsb_release -si)" == "Ubuntu" ]]; then
-			sudo add-apt-repository ppa:neovim-ppa/stable
-			sudo apt-get update
-			sudo apt-get install -qy neovim python3-dev python3-pip \
-                python-pip libffi-dev tree zsh curl tmux tree
-			mkdir -p ~/.local/share/fonts
-            curl https://pyvenv.run | bash
-		elif [[ "$(lsb_release -si)" == "ManjaroLinux" ]]; then
-			sudo pacman -S --noconfirm zsh curl tmux
-		fi
-	elif [[ "$(uname)" == "Darwin" ]]; then
-		if [ ! -f "$(which brew)" ]; then
-			brew install zsh curl tmux
-		fi
-	fi
+if [ "$(uname)" == "Linux" ]; then
+    if [[ "$(lsb_release -si)" == "Ubuntu" ]]; then
+        sudo apt-add-repository ppa:fish-shell/release-4
+        sudo apt-get update
+        sudo apt-get install -qy neovim python2-dev python3-pip \
+            python-pip libffi-dev curl tmux tree
+        mkdir -p ~/.local/share/fonts
+    elif [[ "$(uname)" == "Darwin" ]]; then
+        if [ ! -f "$(which brew)" ]; then
+            brew install fish curl tmux httpie
+        fi
+    fi
+fi
+
+if [ "$ans" == "zsh" ]; then
 
 	export ZSH="$HOME/.file/oh-my-zsh"
+    sudo apt-get install zsh
 
 	if [ ! -d "$ZSH" ]; then
 		echo 'y' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -29,35 +27,25 @@ if [[ "$input" == "zsh" ]]; then
         export SHELL=/usr/bin/zsh
 	fi
 
-elif [[ "$input" == "fish" ]]; then
-	if [[ "$(uname)" == "Linux" ]]; then
-		if [[ "$(lsb_release -si)" == "Ubuntu" ]]; then
-			sudo apt-add-repository ppa:fish-shell/release-3
-			sudo apt-get update
-			sudo apt-get install -qy neovim python3-dev python3-pip \
-                python-pip libffi-dev tree fish curl tmux tree
-			mkdir -p ~/.local/share/fonts
-	elif [[ "$(uname)" == "Darwin" ]]; then
-		if [ ! -f "$(which brew)" ]; then
-			brew install fish curl tmux
-		fi
-	fi
+elif [ "$ans" == "fish" ]; then
 
 	export FISH="$HOME/.file/oh-my-fish"
+    sudo apt-get install fish
 
 	if [ ! -d "$FISH" ]; then
 		echo 'y' | curl -L https://get.oh-my.fish | fish
 		echo /usr/local/bin/fish | sudo tee -a /etc/shells
 		chsh -s /usr/local/bin/fish
         export SHELL=/usr/local/bin/fish
+    fi
 
 else
     echo "Choose either zsh or fish, exiting..."
     exit 1
 fi
 
-printf "Installing symlinks...\n"
+echo "Installing symlinks..."
 /bin/bash ./symlink.sh
 /bin/bash ./plugin.sh
-printf "Remember to set your patched font."
+echo "Remember to set your patched font."
 exec $SHELL
