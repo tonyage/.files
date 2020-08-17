ed34867e8e0e53330d061f19c2ce707e1e5952fa
Tony Do <tonyttdo@gmail.com>
2020-08-17 06:29:39 -0500

    don't care about supporting bash anymore, deleting deprecated .nvimrc and adding some more rules to .ctags
    install script incorrectly exits after omz install

--- bin/install.sh
+++ bin/install.sh
@@ -52,35 +52,8 @@ case "$ans" in
 		;;
 esac
 
-plugin_dir="$ZSH/custom/plugins"
-themes_dir="$ZSH/custom/themes"
-
-# Rust 
-cargo install exa bat
-
 printf "Installing symlinks and zsh custom plugins..."
-urls=("https://github.com/zsh-users/zsh-syntax-highlighting.git"
-		"https://github.com/chrissicool/zsh-256color"
-		"https://github.com/zsh-users/zsh-autosuggestions"
-		"https://github.com/hlissner/zsh-autopair"
-		"https://github.com/zsh-users/zsh-history-substring-search.git")
-	
-if [ -d "$plugin_dir" ] && [ -d "$themes_dir" ]; then
-    printf "installing zsh plugins....\n"
-    pushd $plugin_dir > /dev/null
-    for url in "${urls[@]}"; do
-        git clone $url
-    done
-    popd > /dev/null
-
-    pushd $themes_dir > /dev/null
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
-    popd > /dev/null
-else 
-    printf("zsh plugins and/or themes directories not found.")
-    exit 1
-fi
-
 /bin/bash ./symlink.sh
+/bin/bash ./plugin.sh
 printf "Remember to set patched font. MesloLGS is really good (https://github.com/romkatv/powerlevel10k#fonts)"
 exec $SHELL
---
+++ bin/plugin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+plugin_dir="$ZSH/custom/plugins"
+themes_dir="$ZSH/custom/themes"
+urls=("https://github.com/zsh-users/zsh-syntax-highlighting.git"
+		"https://github.com/chrissicool/zsh-256color"
+		"https://github.com/zsh-users/zsh-autosuggestions"
+		"https://github.com/hlissner/zsh-autopair"
+		"https://github.com/zsh-users/zsh-history-substring-search.git")
+	
+if [ -d "$plugin_dir" ] && [ -d "$themes_dir" ]; then
+    printf "installing zsh plugins....\n"
+    pushd $plugin_dir > /dev/null
+    for url in "${urls[@]}"; do
+        git clone $url
+    done
+    popd > /dev/null
+
+    pushd $themes_dir > /dev/null
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+    popd > /dev/null
+else 
+    printf("zsh plugins and/or themes directories not found.")
+    exit 1
+fi
+
+# Rust 
+cargo install exa bat
